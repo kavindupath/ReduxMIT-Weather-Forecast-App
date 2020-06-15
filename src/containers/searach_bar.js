6819d06b11fe0bed1ee4dcd22c680407ccc6978e
kavindu <kavindupath@gmail.com>
2020-06-15 15:52:36 +0530

    Controlled component and binding context

--- src/containers/searach_bar.js
+++ src/containers/searach_bar.js
@@ -1,10 +1,29 @@
 import React from "react";
 
 export default class SearchBar extends React.Component {
-  render() {
+  constructor(props){
+      super(props);
+
+      this.state={term:""};
+      this.onInputChange=this.onInputChange.bind(this);
+  }
+
+  onInputChange(event)
+  {
+      console.log(event.target.value);
+      this.setState({term:event.target.value});
+  }
+  
+    render() {
     return (
       <form className="input-group">
-        <input />
+        <input 
+        placeholder="Get a five day forecat in your fav cities"
+        className="form-control"
+        value={this.state.term}
+        onChange={this.onInputChange}
+        />
+
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             Submit
