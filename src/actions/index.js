871e8242365ab892cef77be560f77f217f7ec618
kavindu <kavindupath@gmail.com>
2020-06-15 23:39:49 +0530

    Ajax request with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from "axios";
+
 const API_KEY="39aeb7e2d0f27164e55527e7feeec63c";
+const ROOT_URL=`https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+//or you can use like this
+//const ROOT_URL="https://samples.openweathermap.org/data/2.5/forecast?appid="+ API_KEY;
+
+export function FetchWeather(city)
+{
+
+    const url=`${ROOT_URL}&q=${city},us`;
+    const request=axios.get(url);
+
+    return{
+        type:"FETCH_WEATHER",
+        payload: request
+
+    };
+}
